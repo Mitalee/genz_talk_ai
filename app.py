297ee6b47ce4a15a24c917466b4045aeef6f4f8e
Mitalee <raomitalee@gmail.com>
2023-10-26 12:46:09 +0530

    trying to remove system message

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = context.copy() #[]
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
