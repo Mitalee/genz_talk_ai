cfd50b5589b6b17b6861ef812432fde2115d3cb2
Mitalee <raomitalee@gmail.com>
2023-10-26 12:43:07 +0530

    Saving app chat history

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ if "messages" not in st.session_state:
     st.session_state.messages = context.copy() #[]
 
 # Display chat messages from history on app rerun
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 # Accept user input
 prompt = st.chat_input("What is up?")
